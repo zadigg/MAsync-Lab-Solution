2532650f72690cab1eb38d67614359ac49e730cb
zadigg <abelkibebe5@gmail.com>
2022-11-09 16:39:20 -0600

    Fixed calculator/index.html ✔️✔️

---
+++ Lab6/imageSwap/swap.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.onload = function () {
+  document.getElementById("btn1").onclick = swapImage;
+};
+function swapImage() {
+  let image1 = document.getElementById("image1");
+  let image2 = document.getElementById("image2");
+  let temp = image1.src;
+  image1.src = image2.src;
+  image2.src = temp;
+}
