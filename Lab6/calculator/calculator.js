bf8dd4c9de2867c5153312ffb32ece512c5c02c1
zadigg <abelkibebe5@gmail.com>
2022-11-10 10:34:19 -0600

    Commited: 🌞 calculator/calculator.js 🌞

--- Lab6/calculator/calculator.js
+++ Lab6/calculator/calculator.js
@@ -1,19 +1,17 @@
-"use strict"
-
-window.onload = function (){
-    document.getElementById("add").onclick = add;
-    document.getElementById("subtract").onclick = subtract;
-}
+window.onload = function () {
+  document.getElementById("add").onclick = add;
+  document.getElementById("subtract").onclick = subtract;
+};
 
 //
-function add(){
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-    document.getElementById("result").value= number1+number2; 
+function add() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 + number2;
 }
 //
-function subtract(){
-    let number1 = Number(document.getElementById("number1").value);
-    let number2 = Number(document.getElementById("number2").value);
-    document.getElementById("result").value = number1 - number2;
+function subtract() {
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+  document.getElementById("result").value = number1 - number2;
 }
