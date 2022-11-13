e7d34a24d638a0993d2751e9f5d7591fa3f0806e
zadigg <abelkibebe5@gmail.com>
2022-11-13 15:58:31 -0600

    Fixed  ✔️✔️

---
+++ Lab7/exercise2.js
@@ -0,0 +1,15 @@
+Array.prototype.removeDuplicatesAsync = function () {
+  let arr = this;
+  new Promise(function (resolve, reject) {
+    resolve([...new Set(arr)]);
+  }).then(console.log);
+};
+
+console.log("start");
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync();
+console.log("end");
+
+//Output......................
+//start
+//end
+//[4,1,5,7,2,3,6]
